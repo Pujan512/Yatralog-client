74c25b9d10da8dedeb8af13548bab66b7bfd2a38
Pujan <shresthapujan0010@gmail.com>
2025-05-24 14:47:17 +0545

    validate blog form

--- app/components/BlogForm.jsx
+++ app/components/BlogForm.jsx
@@ -2,22 +2,41 @@
 import React, { useState } from 'react'
 import { redirect } from 'next/navigation';
 import { useAuthStore } from '../lib/store/useAuthStore';
+import toast from 'react-hot-toast';
 
-const BlogForm = ({id, btnText, formAction, title, description}) => {
+const BlogForm = ({ id, btnText, formAction, title, description }) => {
 
   const [formData, setFormData] = useState({
     title: title ?? "",
-    description:description ?? "",
+    description: description ?? "",
     images: []
   })
 
   const handleImageChange = (e) => {
-    setFormData({...formData, images: Array.from(e.target.files)});
+    setFormData({ ...formData, images: Array.from(e.target.files) });
+  }
+
+  const validate = () => {
+    if (formData.title.length < 3) {
+      toast.error("Title must be at least 3 characters long.");
+      return false;
+    }
+
+    if (formData.description.length < 10) {
+      toast.error("Description must be at least 10 characters long.");
+      return false;
+    }
+
+    if (formData.images.length > 5) {
+      toast.error("Maximum images limit: 5");
+      return false;
+    }
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+    validate();
+
     const formDataToSend = new FormData();
     formDataToSend.append('title', formData.title);
     formDataToSend.append('description', formData.description);
@@ -25,21 +44,29 @@ const BlogForm = ({id, btnText, formAction, title, description}) => {
       formDataToSend.append('images', image);
     })
 
-    //dynamic function post or edit
     const res = await formAction(formDataToSend);
-    if(res.success) btnText ==='Add' ? redirect('/blogs') : redirect('/blogs/'+id);
+    if (res.success) btnText === 'Add' ? redirect('/blogs') : redirect('/blogs/' + id);
   }
 
-  const {authUser} = useAuthStore();
-  if(!authUser) redirect('/login');
+  const { authUser } = useAuthStore();
+  if (!authUser) redirect('/login');
 
   return (
-    <section>
-      <form onSubmit={handleSubmit}>
-        <input value={formData.title} placeholder='Title' type="text" name="title" onChange={(e)=>setFormData({...formData, title: e.target.value})} />
-        <textarea className='outline-1' rows={5} cols={30} value={formData.description} placeholder='Description' type="text" name="description" onChange={(e)=>setFormData({...formData, description: e.target.value})} />
-        <input type="file" accept='image/*' multiple={true} onChange={handleImageChange}/>
-        <input type="submit" value={btnText} />
+    <section className='mx-auto my-10'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-5'>
+        <div className='grid grid-cols-2 gap-5'>
+          <input
+            className='outline-1 p-2 focus:outline-blue-500'
+            value={formData.title} placeholder='Title' type="text" name="title" onChange={(e) => setFormData({ ...formData, title: e.target.value })} />
+          <div>Images:<input
+            className='file:mr-4 file:rounded-full file:border-0 file:bg-violet-50 file:px-4 file:py-2 file:text-sm file:font-semibold file:text-violet-700 hover:file:bg-violet-100'
+            type="file" accept='image/*' multiple={true} onChange={handleImageChange} /></div>
+        <span className='text-sm -my-5 text-blue-800 col-start-2'>Note: Max limit - 5 images</span>
+        </div>
+        <textarea
+          className='outline-1 px-4 py-2 focus:outline-blue-500'
+          rows={12} cols={100} value={formData.description} placeholder='Description' type="text" name="description" onChange={(e) => setFormData({ ...formData, description: e.target.value })} />
+        <input className='border-1 px-4 py-2 w-50 cursor-pointer' type="submit" value={btnText} />
       </form>
     </section>
   )
